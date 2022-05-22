fc2c7a9c3eb71be43e3cee03de55f97c6ff1a8ea
sebokdas1 <sebok.das.ff@gmail.com>
2022-05-22 14:48:58 +0600

    get parts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
@@ -7,3 +8,32 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.jxau4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db('istockSources').collection('parts');
+
+        app.get('/part', async (req, res) => {
+            const query = {};
+            const cursor = taskCollection.find(query).project({ name: 1 });
+            const parts = await cursor.toArray();
+            res.send(parts);
+        });
+
+    }
+    finally { }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello istock World!');
+});
+
+app.listen(port, () => {
+    console.log(`istock app listening on port ${port}`);
+});
