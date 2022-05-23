2b1143c6f8404d785b6cb4691acbdb9a82e90540
sebokdas1 <sebok.das.ff@gmail.com>
2022-05-23 19:48:55 +0600

    verify jwt get user and get user by find email

--- index.js
+++ index.js
@@ -13,10 +13,29 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.jxau4.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+//verifying JWT
+function verifyJWT(req, res, next) {
+    console.log('abc')
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         await client.connect();
         const partCollection = client.db('istockSources').collection('parts');
+        const userCollection = client.db('doctorsPortal').collection('users');
 
         //get all parts
         app.get('/part', async (req, res) => {
@@ -32,6 +51,24 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const part = await partCollection.findOne(query);
             res.send(part);
+        });
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            }
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '3h' });
+            res.send({ result, token });
         });
 
 
